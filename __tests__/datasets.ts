17def8d075f6fd3bde5c7fb8b7799383a73758c9
Symegu <89914120+Symegu@users.noreply.github.com>
2024-07-22 12:06:40 +0300

    upd datasets

--- __tests__/datasets.ts
+++ __tests__/datasets.ts
@@ -7,12 +7,12 @@ import {DBType} from '../src/db/db'
 export const video1: any /*VideoDBType*/ = {
     id: Date.now() + Math.random(),
     title: 't' + Date.now() + Math.random(),
-    author: 'a' + Date.now() + Math.random(),
-    canBeDownloaded: true,
-    minAgeRestriction: null,
-    createdAt: new Date().toISOString(),
-    publicationDate: new Date().toISOString(),
-    availableResolution: [Resolutions.P240],
+    // author: 'a' + Date.now() + Math.random(),
+    // canBeDownloaded: true,
+    // minAgeRestriction: null,
+    // createdAt: new Date().toISOString(),
+    // publicationDate: new Date().toISOString(),
+    // availableResolution: [Resolutions.P240],
 }
  
 // ...
