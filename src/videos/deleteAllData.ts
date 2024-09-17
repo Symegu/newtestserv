af3c89970c7592be133b89b993d2f56dda9d8aa7
Symegu <89914120+Symegu@users.noreply.github.com>
2024-09-17 13:44:13 +0300

    fixed deleting videos

--- src/videos/deleteAllData.ts
+++ src/videos/deleteAllData.ts
@@ -1,6 +1,7 @@
 import {Request, Response} from 'express'
+import {db} from '../db/db'
  
 export const deleteAllData = (req: Request, res: Response<any /*OutputVideoType[]*/>) => {
-  // db.videos = []
+  db.videos = []
   res.sendStatus(204)
 }
